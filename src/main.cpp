69943737e6fc5b43da8265b414d7eab1fb1c0051
Wen <wen_eb@hotmail.com>
2025-09-08 08:24:18 -0400

    modified the starter script to simulate a unity game loop experience

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
-#include <glad/glad.h>  // We want OpenGL 4.6, not the default OpenGL 1.0 included in GLFW
+#include <glad/glad.h>
 
-int main(void)
+GLFWwindow *window;
+float r = 239.0f / 255.0f;
+float g = 136.0f / 255.0f;
+float b = 190.0f / 255.0f;
+float a = 1.0f;
+
+int init()
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    // Request OpenGL 4.3 (1.0 loaded by default)
+    //Request OpenGL 4.6 (1.0 loaded by default)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
@@ -26,31 +30,32 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    // Load OpenGL extensions
+    //Load OpenGL extensions
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        return 0;
-    }
+        return -1;
+    
+    return 0; //Success
+}
+void update()
+{
+    /* Render here */
+    glClearColor(r, g, b, a);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+}
+
+int main(void)
+{
+    init();
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
-    {
-        // Colors are represented as fractions between 0.0 and 1.0, so convert using a colour-picker tool accordingly!
-        float r = 239.0f / 255.0f;
-        float g = 136.0f / 255.0f;
-        float b = 190.0f / 255.0f;
-        float a = 1.0f;
-
-        /* Render here */
-        glClearColor(r, g, b, a);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+        update();
 
     glfwTerminate();
     return 0;
