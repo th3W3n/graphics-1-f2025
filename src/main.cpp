0a5ccf02ae0224b12f54f27259cd0f7981f6c385
Wen <wen_eb@hotmail.com>
2025-09-15 21:33:24 -0400

    Added a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,87 @@
 #include "Window.h"
+#include <cstddef>
 #include <glad/glad.h>
+
+struct vec2
+{
+    float x;
+    float y;
+};
+
+struct vec3
+{
+    float x;
+    float y;
+    float z;
+};
+
+struct Vertex
+{
+    vec2 pos;
+    vec3 col;
+};
+
+static const Vertex vertices[3] =
+    {
+        {{-0.6f, -0.4f}, {1.f, 0.f, 0.f}},
+        {{0.6f, -0.4f}, {0.f, 1.f, 0.f}},
+        {{0.f, 0.6f}, {0.f, 0.f, 1.f}}};
+
+static const char *vertex_shader_text =
+    "#version 330\n"
+    "in vec3 vCol;\n"
+    "in vec2 vPos;\n"
+    "out vec3 color;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = vec4(vPos, 0.0, 1.0);\n"
+    "    color = vCol;\n"
+    "}\n";
+
+static const char *fragment_shader_text =
+    "#version 330\n"
+    "in vec3 color;\n"
+    "out vec4 fragment;\n"
+    "void main()\n"
+    "{\n"
+    "    fragment = vec4(color, 1.0);\n"
+    "}\n";
 
 int main(void)
 {
     InitWindow(800, 800, "Graphics Course");
+
+    GLuint vertex_buffer;
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+
+    const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+
+    const GLuint program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    //const GLint mvp_location = glGetUniformLocation(program, "MVP");
+    const GLint vpos_location = glGetAttribLocation(program, "vPos");
+    const GLint vcol_location = glGetAttribLocation(program, "vCol");
+
+    GLuint vertex_array;
+    glGenVertexArrays(1, &vertex_array);
+    glBindVertexArray(vertex_array);
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(Vertex), (void *)offsetof(Vertex, pos));
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+                          sizeof(Vertex), (void *)offsetof(Vertex, col));
 
     while (!WindowShouldClose())
     {
@@ -17,6 +95,10 @@ int main(void)
 
         glClearColor(r, g, b, a);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(program);
+        glBindVertexArray(vertex_array);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         UpdateWindow();
     }
