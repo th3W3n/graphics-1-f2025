bdfcaf50924f308a54beeea91f0c6ebef5d442a6
Wen <wen_eb@hotmail.com>
2025-09-18 15:05:50 -0400

    Added draw call to render white triangle (use spacebar to switch)

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,18 @@ struct Vertex
     vec3 col; //offset of 8 (4 bytes for x and 4 for y) //rgb colors
 };
 
-static const Vertex vertices[3] =
+static const Vertex vertices_rainbow[3] =
     {
-        {{-0.6f, -0.4f}, {1.f, 0.f, 0.f}},
-        {{0.6f, -0.4f}, {0.f, 1.f, 0.f}},
-        {{0.f, 0.6f}, {0.f, 0.f, 1.f}}};
+        {{-0.6f, -0.4f}, {1.0f, 0.0f, 0.0f}},
+        { {0.6f, -0.4f}, {0.0f, 1.0f, 0.0f}},
+        {  {0.0f, 0.6f}, {0.0f, 0.0f, 1.0f}}
+};
+static const Vertex vertices_white[3] =
+    {
+        {{-0.6f, -0.4f}, {1.0f, 1.0f, 1.0f}},
+        { {0.6f, -0.4f}, {1.0f, 1.0f, 1.0f}},
+        {  {0.0f, 0.6f}, {1.0f, 1.0f, 1.0f}}
+};
 
 static const char *vertex_shader_text =
     "#version 330\n"
@@ -49,9 +56,9 @@ int main(void)
 {
     InitWindow(800, 800, "Graphics Course");
 
-    float r = 239.0f / 255.0f;
-    float g = 136.0f / 255.0f;
-    float b = 190.0f / 255.0f;
+    float r = 180.0f / 255.0f;
+    float g = 250.0f / 255.0f;
+    float b = 210.0f / 255.0f;
     float a = 1.0f;
 
     const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
@@ -67,19 +74,38 @@ int main(void)
     glAttachShader(program, fragment_shader);
     glLinkProgram(program);
 
-    GLuint vertex_buffer;
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    GLuint vertex_buffer_rainbow;
+    glGenBuffers(1, &vertex_buffer_rainbow);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_rainbow);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_rainbow), vertices_rainbow, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, GL_NONE); //unbind the buffer
+    //can only have 1 vertex buffer bound at a time, so must unbind to prevent overwriting it
 
-    GLuint vertex_array;
-    glGenVertexArrays(1, &vertex_array);
-    glBindVertexArray(vertex_array);
+    GLuint vertex_buffer_white;
+    glGenBuffers(1, &vertex_buffer_white);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_white);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_white), vertices_white, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, GL_NONE);
 
+    GLuint vertex_array_rainbow;
+    glGenVertexArrays(1, &vertex_array_rainbow);
+    glBindVertexArray(vertex_array_rainbow);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_rainbow);
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, pos));
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, col));
+    glBindVertexArray(GL_NONE);
+
+    GLuint vertex_array_white;
+    glGenVertexArrays(1, &vertex_array_white);
+    glBindVertexArray(vertex_array_white);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_white);
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, pos));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, col));
+    glBindVertexArray(GL_NONE);
 
     int object_index = 0;
 
@@ -92,28 +118,26 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         if (IsKeyPressed(KEY_SPACE))
-        {
             ++object_index %= 5;
-        }
         switch (object_index)
         {
         case 0:
             glUseProgram(program);
-            glBindVertexArray(vertex_array);
+            glBindVertexArray(vertex_array_rainbow);
             glDrawArrays(GL_TRIANGLES, 0, 3);
             break;
         case 1:
             break;
         case 2:
             glUseProgram(program);
-            glBindVertexArray(vertex_array);
+            glBindVertexArray(vertex_array_white);
             glDrawArrays(GL_TRIANGLES, 0, 3);
             break;
         case 3:
             break;
         case 4:
             glUseProgram(program);
-            glBindVertexArray(vertex_array);
+            glBindVertexArray(vertex_array_rainbow);
             glDrawArrays(GL_TRIANGLES, 0, 3);
             break;
         }
