f45b9c4ba85c4f802300285520cbe501da3e2fd3
Wen <wen_eb@hotmail.com>
2025-11-21 02:03:44 -0500

    added ebo and Q key for switching culling faces CW/CCW

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,8 @@
 #include "Window.h"
 #include "raymath.h"
 #include <cstddef>
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
+// #include <cstdio>
+// #include <cstdlib>
 
 struct Vertex
 {
@@ -12,35 +11,39 @@ struct Vertex
     Vector3 col; // offset of 8 (4 bytes for pos.x + 4 bytes for pos.y = 8)
 };
 
-// Assignment 1 object 1 -- white triangle (change these vertex colours from red to white)!!!
-static const Vertex vertices_white[3] =
+static const Vertex vertices_white[] =
     {
         {{-0.6f, -0.4f}, {1.0f, 1.0f, 1.0f}},
         { {0.6f, -0.4f}, {1.0f, 1.0f, 1.0f}},
         {   {0.f, 0.6f}, {1.0f, 1.0f, 1.0f}}
 };
-
-// Assignment 1 object 2 -- rainbow triangle (done for you)
-//static const Vertex vertices_rainbow[3] =
-//{
-//    { { -0.6f, -0.4f }, { 1.0f, 0.0f, 0.0f } },
-//    { {  0.6f, -0.4f }, { 0.0f, 1.0f, 0.0f } },
-//    { {   0.f,  0.6f }, { 0.0f, 0.0f, 1.0f } }
-//};
-
-static const Vector2 vertex_positions[3] =
+static const Vector2 vertex_positions[] =
     {
         {-0.6f, -0.4f},
         { 0.6f, -0.4f},
         {  0.f,  0.6f}
 };
-
-static const Vector3 vertex_colors[3] =
+static const Vector3 vertex_colors[] =
     {
         {1.0f, 0.0f, 0.0f},
         {0.0f, 1.0f, 0.0f},
         {0.0f, 0.0f, 1.0f}
 };
+static const Vertex vertices_quad[] =
+    {
+        {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+        { {0.5f, -0.5f}, {1.0f, 1.0f, 0.0f}},
+        {  {0.5f, 0.5f}, {1.0f, 0.0f, 1.0f}},
+        { {-0.5f, 0.5f}, {0.0f, 1.0f, 1.0f}}
+};
+static int vertices_quad_indices[] =
+    {
+        0, 1, 2,
+        0, 2, 3};
+static int vertices_quad_indices2[] =
+    {
+        0, 1, 2,
+        0, 3, 2};
 
 int main()
 {
@@ -56,62 +59,94 @@ int main()
     GLuint a1_tri_frag = CreateShader(GL_FRAGMENT_SHADER, "./assets/shaders/a1_triangle.frag");
     GLuint a1_tri_shader = CreateProgram(a1_tri_vert, a1_tri_frag);
 
-    GLuint vertex_buffer_rainbow_positions;
-    GLuint vertex_buffer_rainbow_colors;
-    glGenBuffers(1, &vertex_buffer_rainbow_positions);
-    glGenBuffers(1, &vertex_buffer_rainbow_colors);
+    GLuint vbos_rainbow[2];
+    glGenBuffers(2, vbos_rainbow);
+    GLuint vertex_array_rainbow;
+    glGenVertexArrays(1, &vertex_array_rainbow);
+    glBindVertexArray(vertex_array_rainbow);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_rainbow_positions);
+    glBindBuffer(GL_ARRAY_BUFFER, vbos_rainbow[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_positions), vertex_positions, GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_rainbow_colors);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vector2), 0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbos_rainbow[1]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_colors), vertex_colors, GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, GL_NONE); //unbind the buffer
-    // Can only have 1 vertex buffer bound at a time, so must unbind in order to prevent overwriting it
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vector3), 0);
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    //--------------------------------------------
 
     GLuint vertex_buffer_white;
     glGenBuffers(1, &vertex_buffer_white);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_white);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_white), vertices_white, GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, GL_NONE);
-
-    GLuint vertex_array_rainbow;
-    glGenVertexArrays(1, &vertex_array_rainbow);
-    glBindVertexArray(vertex_array_rainbow);
-
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_rainbow_positions);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vector2), 0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_rainbow_colors);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vector3), 0);
-
-    glBindVertexArray(GL_NONE);
 
     GLuint vertex_array_white;
     glGenVertexArrays(1, &vertex_array_white);
     glBindVertexArray(vertex_array_white);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_white);
 
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, pos));
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, col));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
 
-    glBindVertexArray(GL_NONE);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    //--------------------------------------------
+
+    GLuint vbo_quad;
+    glGenBuffers(1, &vbo_quad);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_quad);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_quad), vertices_quad, GL_STATIC_DRAW);
+
+    GLuint ebo_quad;
+    glGenBuffers(1, &ebo_quad);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_quad);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vertices_quad_indices), vertices_quad_indices, GL_STATIC_DRAW);
+
+    GLuint vao_quad;
+    glGenVertexArrays(1, &vao_quad);
+    glBindVertexArray(vao_quad);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, pos));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, col));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    //--------------------------------------------
+
+    GLuint ebo_quad2;
+    glGenBuffers(1, &ebo_quad2);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_quad2);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vertices_quad_indices2), vertices_quad_indices2, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    //--------------------------------------------
 
     int object_index = 0;
+    bool is_cw = false;
 
     GLint u_color = glGetUniformLocation(a1_tri_shader, "u_color");
     GLint u_world = glGetUniformLocation(a1_tri_shader, "u_world");
 
-    Matrix world = world = MatrixIdentity();
+    Matrix world = MatrixIdentity();
 
     // Generally you want to Scale * Rotate * Translate (order matters)!!!
     //world = MatrixRotateZ(30.0f * DEG2RAD) * MatrixTranslate(0.5f, 0.0f, 0.0f);
 
-    /* Loop until the user closes the window */
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
+
+    //--------------------------------------------
+
     while (!WindowShouldClose())
     {
         if (IsKeyPressed(KEY_ESCAPE))
@@ -126,7 +161,12 @@ int main()
 
         if (IsKeyPressed(KEY_SPACE))
         {
-            ++object_index %= 5;
+            ++object_index %= 7;
+        }
+        if (IsKeyPressed(KEY_Q))
+        {
+            is_cw = !is_cw;
+            glFrontFace(is_cw ? GL_CW : GL_CCW);
         }
 
         switch (object_index)
@@ -135,6 +175,7 @@ int main()
             //white triangle
             glUseProgram(a1_tri_shader);
             glUniform3f(u_color, 1.0f, 1.0f, 1.0f);
+            //must use MatrixToFloat to send mat4 since raylib matrix memory is in a different layout than glsl mat4
             glUniformMatrix4fv(u_world, 1, GL_FALSE, MatrixToFloat(MatrixIdentity()));
             glBindVertexArray(vertex_array_white);
             glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -187,6 +228,22 @@ int main()
                 glDrawArrays(GL_TRIANGLES, 0, 3);
             }
             break;
+        case 5:
+            glUseProgram(a1_tri_shader);
+            glUniform3f(u_color, 1.0f, 1.0f, 1.0f);
+            glUniformMatrix4fv(u_world, 1, GL_FALSE, MatrixToFloat(MatrixIdentity()));
+            glBindVertexArray(vao_quad);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_quad);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            break;
+        case 6:
+            glUseProgram(a1_tri_shader);
+            glUniform3f(u_color, 1.0f, 1.0f, 1.0f);
+            glUniformMatrix4fv(u_world, 1, GL_FALSE, MatrixToFloat(MatrixIdentity()));
+            glBindVertexArray(vao_quad);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_quad2);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            break;
         }
 
         // Called at end of the frame to swap buffers and update input
@@ -195,9 +252,12 @@ int main()
 
     glDeleteVertexArrays(1, &vertex_array_rainbow);
     glDeleteVertexArrays(1, &vertex_array_white);
-    glDeleteBuffers(1, &vertex_buffer_rainbow_positions);
-    glDeleteBuffers(1, &vertex_buffer_rainbow_colors);
+    glDeleteVertexArrays(1, &vao_quad);
+    glDeleteBuffers(2, vbos_rainbow);
     glDeleteBuffers(1, &vertex_buffer_white);
+    glDeleteBuffers(1, &vbo_quad);
+    glDeleteBuffers(1, &ebo_quad);
+    glDeleteBuffers(1, &ebo_quad2);
     glDeleteProgram(a1_tri_shader);
     glDeleteShader(a1_tri_frag);
     glDeleteShader(a1_tri_vert);
